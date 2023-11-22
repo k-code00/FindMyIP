3ca24649621715b1dd9c80c8561c6ab803daa0df
kojo <kojo.abakah@techconsulting.tech>
2023-11-22 12:49:32 -0500

    completed

--- FindMyIP/ViewModel/IPInformationViewModel.swift
+++ FindMyIP/ViewModel/IPInformationViewModel.swift
@@ -14,15 +14,18 @@ class IPInformationViewModel: ObservableObject {
     @Published var isLoading = false
     @Published var showAlert = false
 
+    //fetching IP information.
     func getIPInformation() {
-        self.isLoading = true
+        self.isLoading = true  //start of a network request
         fetchIPInformation { [weak self] result in
             DispatchQueue.main.async {
-                self?.isLoading = false
+                self?.isLoading = false  //end of a network request
                 switch result {
                 case .success(let info):
+                    //if successful, update ipInformation
                     self?.ipInformation = info
                 case .failure(let error):
+                    //if failure, update the errorMessage and set showAlert to true
                     self?.errorMessage = error.localizedDescription
                     self?.showAlert = true
                 }
