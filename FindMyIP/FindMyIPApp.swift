8c0cc09cc28ffc2934455644d9ced49936bda85a
kojo <kojo.abakah@techconsulting.tech>
2023-11-21 19:18:11 -0500

    set up architecture

---
+++ FindMyIP/FindMyIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyIPApp.swift
+//  FindMyIP
+//
+//  Created by Consultant on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindMyIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
